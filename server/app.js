27b416e0872182468573d1a988cbe370964c4bb3
unknown <orhan.hajderovic@hotmail.com>
2017-09-18 20:30:10 +0200

    Mongoose connection configured

--- server/app.js
+++ server/app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('./config/mongoose');
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
