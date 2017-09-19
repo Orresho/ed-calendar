62a343c7cbb767761f6ec6ab8b4476aa5a7d76f7
Carl Jonsson <>
2017-09-19 11:47:05 +0200

    changes

--- client/src/app/start/header.component.ts
+++ client/src/app/start/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Input } from '@angular/core';
 
 @Component({
     selector: 'app-header',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
 })
 
 export class HeaderComponent{
+    
+    @ViewChild('sidenav') sidenav: any;
+
+        //exapanded:boolean;
+    sideWidth:any = 0;
+
+    openNav(event) {
+       // console.log(event);
+        //this.sidenav.nativeElement.style.width = "250px";
+        this.sideWidth = 250;
+    } 
+    closeNav() {
+        //this.sidenav.nativeElement.style.width = "0";
+        this.sideWidth = 0;        
+    }
 
 }
