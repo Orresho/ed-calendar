3d5eb12291950e6ad209b71db30359ce79a5dca9
unknown <orhan.hajderovic@hotmail.com>
2017-09-27 15:33:05 +0200

    backend fetching data from mongo

--- server/app.js
+++ server/app.js
@@ -26,8 +26,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
 app.use('/events', events);
+app.use('/', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
