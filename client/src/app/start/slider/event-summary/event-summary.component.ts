89df600b31a4f90eabda4723d9484d7f97b2f8fc
Carl Jonsson <>
2017-09-28 15:44:00 +0200

    Damn son! that ass

--- client/src/app/start/slider/event-summary/event-summary.component.ts
+++ client/src/app/start/slider/event-summary/event-summary.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { SliderComponent } from '../slider.component';
 
 @Component({
   selector: 'app-event-summary',
   templateUrl: './event-summary.component.html',
   styleUrls: ['./event-summary.component.css']
 })
-export class EventSummaryComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class EventSummaryComponent{
+    temp:  SliderComponent = new SliderComponent();
 
 }
