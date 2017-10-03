49ca745a3353f28ee72511ce8807da5e82c4f4ea
Sebastian Algrim <sa222xu@student.lnu.se>
2017-10-03 09:36:05 +0200

    ändrat databasnamnet

--- server/model/event.js
+++ server/model/event.js
@@ -4,13 +4,13 @@ var Schema = mongoose.Schema;
 var eventSchema = new Schema({
     title: {type: String, required: true},
     imageUrl: {type: String, required: true},
-    subHeading: {type: String},
+    //subHeading: {type: String},
     description: {type: String, required: true},
     price: {type: String, required: true},
     language: {type: String},
     date: {type: String, required: true},
-    time: {type: String, required: true},
-    location: {type: String, required: true},
+    //time: {type: String, required: true},
+    //location: {type: String, required: true},
     speakerImageUrl: {type: String, required: true},
     speakerFullName: {type: String, required: true},
     speakerDescription: {type: String},
@@ -18,4 +18,4 @@ var eventSchema = new Schema({
     speakerPhone: {type: String}
 });
 
-module.exports = mongoose.model('Event', eventSchema);
+module.exports = mongoose.model('Seminars', eventSchema);
