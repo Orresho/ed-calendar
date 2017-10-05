3210ac3e0a08bb286890946b5b8aa160d69aaeb7
unknown <orhan.hajderovic@hotmail.com>
2017-10-05 15:15:36 +0200

    refresh carousel

--- client/src/app/start/slides/slide.component.ts
+++ client/src/app/start/slides/slide.component.ts
@@ -1,31 +1,37 @@
 import { SlideService } from './slide.service';
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, AfterViewInit, OnChanges } from '@angular/core';
 
 //Import image interface
 import { Slide } from './slide.model';
+declare var jQuery;
 
 @Component({
     selector: 'app-slide',
     templateUrl: './slide.component.html',
     styleUrls: ['slide.component.css'],
 })
-export class SlideComponent implements OnInit {
+export class SlideComponent implements OnInit, OnChanges {
     slides: Slide[];
     @Input() speedes: string;
 
-
-    // https://www.google.ba/search?q=property+bind+custom+attributes&rlz=1C1TIGY_enSE758SE758&oq=property+bind+custom+attributes&aqs=chrome..69i57.5675j0j4&sourceid=chrome&ie=UTF-8
     constructor(private slideService: SlideService){
     }
     
     ngOnInit(){
+        console.log("init - " + this.speedes)
+        
         this.slideService.getSlides()
             .subscribe((slides: Slide[]) => {
                 return this.slides = slides;
-                // console.log(slides);
             });
     }
-
-    
-    
+   
+    ngOnChanges() {
+        console.log("change - " + this.speedes)
+        jQuery('#myCarousel').carousel({
+            dataInterval: this.speedes
+        });
+        
+        jQuery('#myCarousel').carousel('cycle');
+    }
 }
