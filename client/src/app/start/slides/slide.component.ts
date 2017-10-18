bbe5ca40e1d9468593955a065667e6ca9e7f18f0
Olle Bogren <ob222dj@student.lnu.se>
2017-10-18 12:42:25 +0200

    adding-share.service

--- client/src/app/start/slides/slide.component.ts
+++ client/src/app/start/slides/slide.component.ts
@@ -13,6 +13,7 @@ declare var jQuery;
 export class SlideComponent implements OnInit, OnChanges {
     slides: Slide[];
     @Input() speedes: string;
+    @Input() selectedValue: string;
 
     constructor(private slideService: SlideService) { }
 
@@ -27,7 +28,8 @@ export class SlideComponent implements OnInit, OnChanges {
 
     // Get Seminars
     getSeminars() {
-        // console.log("init - " + this.speedes)
+       // console.log(this.selectedValue)
+        //console.log("init - " + this.speedes)
         this.slideService.getSeminars()
             .subscribe((slides: Slide[]) => {
                 this.slides = [];
@@ -35,7 +37,7 @@ export class SlideComponent implements OnInit, OnChanges {
                     let dateString = slide['numericDate'];
                     let newDate = new Date(dateString);
                     let today = new Date(Date.now());
-                    if (slide['location'] == 'Växjö') {
+                    if (slide['location'] == this.selectedValue) {
                         if (newDate > today) {
                             console.log(this.slides);
                                 this.slides.push(slide);
@@ -68,7 +70,7 @@ export class SlideComponent implements OnInit, OnChanges {
                     let newDate = new Date(dateString);
                     let today = new Date(Date.now());
                     if (slide['courseInformation'].includes('Växjö')) {
-                        
+                            
                             this.slides.push(slide);
                     }
                     
